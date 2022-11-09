d200100e0d1cb51335ec85c665483ae8715ae13a
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2022-11-09 15:21:30 +0000

    bookers7bまで

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,8 @@ Rails.application.routes.draw do
   resources :chats, only:[:show,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+# ゲストログイン機能実装
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
